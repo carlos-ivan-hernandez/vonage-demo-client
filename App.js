a70de272d73dc461a036034b07eec299c88010b3
Carlos Ivan Hernandez <cihernandez162@gmail.com>
2023-06-02 01:01:15 -0500

    Install dependencies

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const call = () => {
+    console.log('calling');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
+      <Button title="Call" onPress={call} />
     </View>
   );
 }
